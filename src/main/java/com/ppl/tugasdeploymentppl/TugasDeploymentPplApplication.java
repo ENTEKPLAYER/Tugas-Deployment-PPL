e725b12bc9d7dc3c36b3d0d1e3d93294cd8627ba
alin.fathul <alin.fathul@ui.ac.id>
2023-09-08 10:52:11 +0700

    first commit

---
+++ src/main/java/com/ppl/tugasdeploymentppl/TugasDeploymentPplApplication.java
@@ -0,0 +1,13 @@
+package com.ppl.tugasdeploymentppl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasDeploymentPplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TugasDeploymentPplApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ppl/tugasdeploymentppl/TugasDeploymentPplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppl.tugasdeploymentppl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasDeploymentPplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
